cfd43ba287ab280b66b572471a5bda25479ee1d2
Travis <mckinstrytg@gmail.com>
2021-01-05 14:58:27 -0700

    Initial commit

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+import requests
+from django.shortcuts import render
+
+
+def home(request):
+   # get the list of todos
+   response = requests.get('https://jsonplaceholder.typicode.com/todos')
+   # transfor the response to json objects
+   todos = response.json()
+   return render(request, "main_app/home.html", {"billy": todos})
