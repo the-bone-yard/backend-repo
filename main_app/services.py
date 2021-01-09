fab236eba175b91ad1ccce65ff7a318269d61027
Travis <mckinstrytg@gmail.com>
2021-01-08 17:11:14 -0700

    Testing .gitignore file

--- main_app/services.py
+++ main_app/services.py
@@ -1,15 +1,27 @@
 from django.http import JsonResponse
 import requests
+import json
+from main_app.models import Park
 
 def get_parks(self, coordinates='', query=''):
-
+    API_KEY = config('KEY')
     radius = 2000
-    endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAb6K8lFSR2jjUv5GwqKvTMgdumdWPh-FE&keyword=%s&location=%s&radius=2000" % (query,coordinates)
+    endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?fields=photos,formatted_address,name,rating,opening_hours,geometry&key=%s&keyword=dog+park&location=%s&radius=2000" % (API_KEY,coordinates)
 
-    info = requests.get(endpoint_url).json()
+    data = format_data(requests.get(endpoint_url).json())
 
-    data = {
-    'test': info["results"]
-    }
-    # import code; code.interact(local=dict(globals(), **locals()))
-    return JsonResponse(data)
+    return JsonResponse(data, safe=False)
+
+def format_data(info):
+    parks = []
+    for park in info['results']:
+        # if 'opening_hours' in park:
+        data = {
+            'formatted_address': park['vicinity'],
+            'geometry': park['geometry'],
+            'name': park['name'],
+            'opening_hours': park['opening_hours'] if 'opening_hours' in park else 'CLOSED',
+            'photos': park['photos'],
+            'rating': park['rating']}
+        parks.append(data)
+    return parks
--- main_app/tests/test_app.py
+++ main_app/tests/test_app.py
@@ -2,9 +2,17 @@ from django.test import TestCase
 import requests
 
 
-class AnimalTestCase(TestCase):
+class ApiTestCase(TestCase):
 
     def test_can_make_api_call(self):
+        """Can make an API call and expect specific attributes"""
+        data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
+        # import code; code.interact(local=dict(globals(), **locals()))
+        self.assertEqual(str(type(data)), "<class 'dict'>")
+        self.assertEqual(data['userId'], 1)
+        self.assertEqual(str(data.keys()), "dict_keys(['userId', 'id', 'title', 'completed'])")
+
+    def test_can_reformat_json_object(self):
         """Can make an API call and expect specific attributes"""
         data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
         # import code; code.interact(local=dict(globals(), **locals()))
