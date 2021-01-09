89ae1667cb72d2e0ab73fbd69cf4793ee656b542
Travis <mckinstrytg@gmail.com>
2021-01-08 19:55:26 -0700

    Bug fix for environmental variables

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Park():
-    park_info = ''
-    def __init__(self, park):
-        self.park_info = park
-        # import code; code.interact(local=dict(globals(), **locals()))
