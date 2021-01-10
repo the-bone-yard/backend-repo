ce428a058013e6aeffcc30ac19ae69329ce3882e
Travis <mckinstrytg@gmail.com>
2021-01-10 12:25:48 -0700

    Setting cors values to allow FE to access endpoint

--- main_app/services.py
+++ main_app/services.py
@@ -2,20 +2,27 @@ from django.http import JsonResponse
 import requests
 import json
 from decouple import config
+import code
+import re
 
 def get_parks(self, coordinates=''):
     KEY = config('KEY')
     radius = 2000
-    endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?fields=photos,formatted_address,name,rating,opening_hours,geometry&key=%s&keyword=dog+park&location=%s&radius=2000" % (KEY,coordinates)
+    if len(coordinates.split(',')[0]) > 1 and float(coordinates.split(',')[0]):
+        endpoint_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?fields=photos,formatted_address,name,rating,opening_hours,geometry&key={KEY}&keyword=dog+park&location={coordinates}&radius=2000"
+        data = format_data(requests.get(endpoint_url).json())
+    elif re.match(r'^-?\d+(?:\.\d+)?$', coordinates.split(',')[0]) is None:
+        info = {'info': "geocode_api"}
+        data = format_directions(requests.get(endpoint_url).json())
+    else:
+        data = {'Error': "Input is incorrect. Please enter either coordinates as '132.5543,-204.5566' or 'city,state'"}
 
-    data = format_data(requests.get(endpoint_url).json())
-    # import code; code.interact(local=dict(globals(), **locals()))
     return JsonResponse(data, safe=False)
 
 def format_data(info):
     parks = []
     for park in info['results']:
-        # if 'opening_hours' in park:
+        code.interact(local=dict(globals(), **locals()))
         data = {
             'formatted_address': park['vicinity'],
             'geometry': park['geometry'],
@@ -25,3 +32,6 @@ def format_data(info):
             'rating': park['rating']}
         parks.append(data)
     return parks
+
+def format_directions(info):
+    'dude'
--- main_app/tests/test_app.py
+++ main_app/tests/test_app.py
@@ -16,6 +16,6 @@ class ApiTestCase(TestCase):
         """Can make an API call and expect specific attributes"""
         data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
         # import code; code.interact(local=dict(globals(), **locals()))
-        self.assertEqual(str(type(data)), "<class 'dict'>")
+        self.assertEqual(type(data), dict)
         self.assertEqual(data['userId'], 1)
         self.assertEqual(str(data.keys()), "dict_keys(['userId', 'id', 'title', 'completed'])")
