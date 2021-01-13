23d2b5c36f7e614f69b6d6e5446d61f10b41db21
Travis McKinstry <mckinstrytg@gmail.com>
2021-01-12 19:46:41 -0700

    Change DB to postgresql

--- boneyardproj/settings.py
+++ boneyardproj/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'boneyardproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'boneyard',
     }
 }
 
@@ -142,4 +142,3 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STOREAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
